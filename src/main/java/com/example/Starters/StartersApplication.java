d979f0bf802f3281f2b4cc60b598f06be2924a6e
VMOHANAV <vmohanav@ford.com>
2020-01-25 06:37:07 +0530

    Initial Commit

---
+++ src/main/java/com/example/Starters/StartersApplication.java
@@ -0,0 +1,13 @@
+package com.example.Starters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Starters/StartersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Starters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
